dc97256dd68c040076d25f61fb1c88ecf9c65533
Tom Scott <tscott@weblinc.com>
2015-09-26 15:28:52 -0400

    Rename revisions controller

---
+++ app/controllers/genres/revisions_controller.rb
@@ -0,0 +1,18 @@
+class Genres::RevisionsController < ApplicationController
+  expose :genre
+  expose :revision, ancestor: :genre
+
+  def new
+    render :new
+  end
+
+  def create
+    revision.save
+    respond_with revision
+  end
+
+  def update
+    revision.update approved: params[:revision][:approved]
+    respond_with revision
+  end
+end
